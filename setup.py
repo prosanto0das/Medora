3749d98b46c6a0c35850161b62f159fd86eef125
prosanto0das <prosanto0das23@gmail.com>
2026-09-11 23:38:02 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medora",
+    version = "0.1.0",
+    author= "Prosanto Das",
+    author_email= "prosanto0das23@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
